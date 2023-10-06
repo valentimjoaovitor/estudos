2d6dee11233f01959557c218754299ff45cc0232
João Vitor Valentim <147115100+valentimjoaovitor@users.noreply.github.com>
2023-10-05 22:26:55 -0300

    Update for_loops11.py

--- python_work/Capitulo_4/for_loops11.py
+++ python_work/Capitulo_4/for_loops11.py
@@ -1,3 +1,17 @@
 '''
 4.11 Minhas pizzas, suas pizzas pag 102
 '''
+
+tipos_pizza = ['pepperoni', 'portuguesa', 'frango']
+friend_pizza = tipos_pizza[:]
+
+tipos_pizza.append('doce')
+friend_pizza.append('nutella')
+
+print('Minhas pizzas favoritas são: ')
+for pizzas in tipos_pizza:
+    print(pizzas)
+
+print('\nMinhas pizzas favoritas são: ')
+for pizzas_do_amigos in friend_pizza:
+    print(pizzas_do_amigos)
