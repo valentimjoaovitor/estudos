65405730c3c7a4cc68b7d65f53dca1c30f68252a
João Vitor Valentim <147115100+valentimjoaovitor@users.noreply.github.com>
2023-10-08 14:24:31 -0300

    Add files via upload

---
+++ python_work/capitulo_5/atividade3.py
@@ -0,0 +1,11 @@
+'''
+5.3 cores alienígenas #1 página 124
+'''
+
+alien_color = 'green'
+
+if alien_color == 'green':
+    print('Você acabou de receber 5 pontos')
+
+if alien_color == 'red':
+    print('você recebeu -1 ponto')
---
+++ python_work/capitulo_5/atividade4.py
@@ -0,0 +1,29 @@
+'''
+exercício página 124
+5.4 Cores alienígenas #2
+'''
+
+
+alien_color = 'verde'
+pontuacao = 0
+
+print('primeiro código')
+if alien_color == 'verde':
+    print('você acabou de ganhar 5 pontos')
+    pontuacao += 5
+
+else:
+    print('Você acabou de receber 10 pontos')
+    pontuacao += 10
+
+print('\nSegundo código')
+color_alien = 'vermelho'
+pontuation = 0
+
+if color_alien == 'verde':
+    print('você acabou de ganhar 5 pontos')
+    pontuation += 5
+
+else:
+    print('Você acabou de receber 10 pontos')
+    pontuation += 10
