3ebb572f150b281b2b3eed333d451a84ce232638
João Vitor Valentim <147115100+valentimjoaovitor@users.noreply.github.com>
2023-11-08 22:20:52 -0300

    Update person.py

--- python_work/capitulo_8/person.py
+++ python_work/capitulo_8/person.py
@@ -1,7 +1,17 @@
-def build_person(first_name, last_name):
+# def build_person(first_name, last_name):
+#     '''Retorna um dicionário de informações sobre uma pessoa'''
+#    person = {'first': first_name, 'last': last_name}
+#     return person
+
+# musician = build_person('jimi', 'hendrix')
+# print(musician)
+
+def build_person(first_name, last_name, age=None):
     '''Retorna um dicionário de informações sobre uma pessoa'''
     person = {'first': first_name, 'last': last_name}
+    if age:
+        person['age'] = age
     return person
 
-musician = build_person('jimi', 'hendrix')
-print(musician)
+musician = build_person('jimi', 'hendrix', age=27)
+print(musician
